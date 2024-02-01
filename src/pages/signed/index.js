f0bbfb87f45ca947d820cb79cc12866544383ec2
Naufal Faiq Azryan <naufalazryan05@gmail.com>
2024-02-02 00:37:38 +0800

    Signed Table

--- src/pages/signed/index.js
+++ src/pages/signed/index.js
@@ -1,125 +1,176 @@
-import React, { useState, useEffect, useRef , useCallback} from "react";
-import Image from "next/image";
-import Logo from "@/../../public/images/logo.png";
-import { FaVolumeUp } from "react-icons/fa";
-import axios from "axios";
-import { useRouter } from "next/router";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState, useEffect, useRef, useCallback } from "react"
+import Image from "next/image"
+import Logo from "@/../../public/images/logo.png"
+import { FaVolumeUp } from "react-icons/fa"
+import axios from "axios"
+import { useRouter } from "next/router"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { motion, useAnimation } from 'framer-motion'
+
 
 const withAuth = (WrappedComponent) => {
-   return (props) => {
-    const router = useRouter();
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  return (props) => {
+    const router = useRouter()
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
     const token =
-      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+      typeof window !== "undefined" ? localStorage.getItem("token") : null
 
     useEffect(() => {
       if (token) {
-        setIsAuthenticated(true);
+        setIsAuthenticated(true)
       } else {
-        setIsAuthenticated(false);
-        router.push('/auth/login');
+        setIsAuthenticated(false)
+        router.push('/auth/login')
       }
-    }, [router, token]);
+    }, [router, token])
 
-    return isAuthenticated ? <WrappedComponent {...props} /> : null;
-  };
-};
+    return isAuthenticated ? <WrappedComponent {...props} /> : null
+  }
+}
 
 const Signed = React.memo(() => {
+
   const Table = () => {
     const data = [
       {
         kelas: "X",
         jadwal: [
-          { jam: "1", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "2", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "3", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "4", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
+          { jam: "1", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "2", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "3", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "4", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "5", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "6", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "7", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "8", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "9", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
+          { jam: "10", a: "PWdPB", b: "PWdPB", c: "PWdPB", d: "PWdPB", e: "PWdPB", f: "PWdPB", g: "PWdPB" },
         ],
       },
       {
         kelas: "XI",
         jadwal: [
-          { jam: "1", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "2", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "3", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "4", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
+          { jam: "1", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "2", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "3", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "4", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "5", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "6", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "7", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "8", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "9", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
+          { jam: "10", a: "PKK", b: "PKK", c: "PKK", d: "PKK", e: "PKK", f: "PKK", g: "PKK" },
         ],
       },
       {
         kelas: "XII",
         jadwal: [
-          { jam: "1", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "2", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "3", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
-          { jam: "4", a: "Data", b: "Data", c: "Data", d: "Data", e: "Data", f: "Data", g: "Data" },
+          { jam: "1", a: "MTK", b: "MTK", c: "MTK", d: "MTK", e: "MTK", f: "MTK", g: "MTK" },
+          { jam: "2", a: "MTK", b: "MTK", c: "MTK", d: "MTK", e: "MTK", f: "MTK", g: "MTK" },
+          { jam: "3", a: "MTK", b: "MTK", c: "MTK", d: "MTK", e: "MTK", f: "MTK", g: "MTK" },
+          { jam: "4", a: "MTK", b: "MTK", c: "MTK", d: "MTK", e: "MTK", f: "MTK", g: "MTK" },
+          { jam: "5", a: "MTK", b: "MTK", c: "MTK", d: "MTK", e: "MTK", f: "MTK", g: "MTK" },
         ],
       },
     ];
 
-    const tableRef = useRef(null);
+
+
+    const [isFullScreen, setIsFullScreen] = useState(false);
 
     useEffect(() => {
-      const handleScroll = () => {
-        const currentRef = tableRef.current;
-        if (currentRef) {
-          const { clientHeight, scrollHeight, scrollTop } = currentRef;
-          const shouldAutoScroll = scrollHeight - scrollTop <= clientHeight + 10;
-          if (shouldAutoScroll) {
-            currentRef.scrollTop = scrollHeight - clientHeight;
-          }
-        }
+      const handleResize = () => {
+        setIsFullScreen(window.innerHeight === screen.height);
       };
-    
-      const cleanupRef = tableRef.current; // Simpan nilai ref dalam variabel lokal
-      if (cleanupRef) {
-        cleanupRef.addEventListener('scroll', handleScroll);
-      }
-    
+  
+      window.addEventListener('resize', handleResize);
+  
+      setIsFullScreen(window.innerHeight === screen.height);
+  
       return () => {
-        const currentRef = cleanupRef; // Gunakan variabel lokal dalam efek cleanup
-        if (currentRef) {
-          currentRef.removeEventListener('scroll', handleScroll);
-        }
+        window.removeEventListener('resize', handleResize);
       };
-    }, []);  // Hapus ref dari array dependensi karena ref tidak berubah
-    
-    
-
+    }, []);
+  
 
     return (
-      <div className="mx-5 my-4 justify-start overflow-auto">
-        <div className="max-h-screen rounded-tl-xl rounded-bl-xl overflow-y-auto sb-hidden" ref={tableRef}>
-          <table className="border-collapse table-auto text-center">
-            <thead className='relative'>
+      <div className="mx-5 justify-start overflow-hidden max-h-screen" style={{ height: isFullScreen ? '100vh' : 'auto' }}>
+      <div className={`h-[520px] relative max-w-[994px] overflow-y-auto border-b sb-hidden`}>
+        <table className="max-w-screen-md table-auto">
+            <thead className="relative overflow-hidden z-10 ">
               <tr className="bg-merah text-white">
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">Kelas</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">Jam</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">A</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">B</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">C</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">D</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">E</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">F</th>
-                <th className="py-2 px-9 sticky top-0 bg-merah text-white">G</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center ">Kelas</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">Jam</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">A</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">B</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">C</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">D</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">E</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">F</th>
+                <th className="py-2 px-9 sticky top-0 bg-merah text-white text-center">G</th>
               </tr>
             </thead>
-            <tbody className='table-body'>
+            <tbody className='marquee-animation'>
               {data.map((kelasData, kelasIndex) => (
                 kelasData.jadwal.map((row, jamIndex) => (
                   <tr key={`${kelasIndex}-${jamIndex}`}>
                     {jamIndex === 0 && (
-                      <td rowSpan={kelasData.jadwal.length} className="py-2 px-9 border">{kelasData.kelas}</td>
+                      <td
+                        rowSpan={kelasData.jadwal.length}
+                        className={`py-2 px-8  text-center border-x border-b z-1 ${kelasData.kelas} ${kelasData.kelas === 'X' ? '' : ''
+                          }`}
+                      >
+                        {kelasData.kelas}
+                      </td>
                     )}
-                    <td className="py-2 px-9 border">{row.jam}</td>
-                    <td className="py-2 px-9 border">{row.a}</td>
-                    <td className="py-2 px-9 border">{row.b}</td>
-                    <td className="py-2 px-9 border">{row.c}</td>
-                    <td className="py-2 px-9 border">{row.d}</td>
-                    <td className="py-2 px-9 border">{row.e}</td>
-                    <td className="py-2 px-9 border">{row.f}</td>
-                    <td className="py-2 px-9 border">{row.g}</td>
+                    <td
+                      className={`py-2 px-8 text-center border-r  z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.jam}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.a}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.b}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.c}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.d}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center  border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.e}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center  border-r z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.f}
+                    </td>
+                    <td
+                      className={`py-2 px-8 text-center border-r  z-1 ${kelasData.kelas === 'X' && jamIndex === 0 ? '' : ''
+                        }`}
+                    >
+                      {row.g}
+                    </td>
                   </tr>
                 ))
               ))}
@@ -130,8 +181,10 @@ const Signed = React.memo(() => {
     );
   };
 
+
+
   const Navbar = () => {
-    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const [currentDateTime, setCurrentDateTime] = useState(new Date())
 
     const formatDateTime = (date) => {
       const dateOptions = {
@@ -139,43 +192,43 @@ const Signed = React.memo(() => {
         year: "numeric",
         month: "long",
         day: "numeric",
-      };
+      }
 
       const timeOptions = {
         hour: "numeric",
         minute: "numeric",
         second: "numeric"
-      };
+      }
 
-      const dateString = date.toLocaleString("id-ID", dateOptions);
-      const timeString = date.toLocaleString("id-ID", timeOptions);
+      const dateString = date.toLocaleString("id-ID", dateOptions)
+      const timeString = date.toLocaleString("id-ID", timeOptions)
 
       return {
         date: dateString,
         time: timeString,
-      };
-    };
+      }
+    }
 
     useEffect(() => {
       const intervalId = setInterval(() => {
-        setCurrentDateTime(new Date());
-      }, 1000);
+        setCurrentDateTime(new Date())
+      }, 1000)
 
       return () => {
-        clearInterval(intervalId);
-      };
-    }, []);
+        clearInterval(intervalId)
+      }
+    }, [])
 
-    const formattedDateTime = formatDateTime(currentDateTime);
+    const formattedDateTime = formatDateTime(currentDateTime)
 
     return (
-      <div className="grid grid-cols-3 grid-flow-col p-5">
+      <div className="grid grid-cols-3 grid-flow-col p-5 mb-10">
         <div className="text-black font-bold place-self-start text-xl">{formattedDateTime.date}</div>
         <Image src={Logo} alt="SMK Telkom Logo" className="w-32 place-self-center" />
         <div className="text-black font-bold place-self-end text-xl">{formattedDateTime.time}</div>
       </div>
-    );
-  };
+    )
+  }
 
   const Announce = () => {
     const [data, setData] = useState([]);
@@ -196,7 +249,7 @@ const Signed = React.memo(() => {
               },
             }
           );
-  
+
           if (response.data && response.data.data) {
             const { data } = response.data;
             setData(data);
@@ -214,7 +267,7 @@ const Signed = React.memo(() => {
         setIsLoading(false);
       }
     }, [token]);
-  
+
 
     useEffect(() => {
       fetchData();
@@ -253,9 +306,25 @@ const Signed = React.memo(() => {
       fetchData();
     }, 3 * 60 * 1000);
 
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsFullScreen(window.innerHeight === screen.height);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      setIsFullScreen(window.innerHeight === screen.height);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     return (
-      <div className="bg-merah text-white text-center p-2 fixed bottom-0 left-0 w-full flex items-center justify-center">
-        <FaVolumeUp className="mr-5" />
+      <div className="bg-merah text-white gap-3 text-center p-2 fixed bottom-0 w-full  flex items-center justify-center" style={{ height: isFullScreen ? '8vh' : 'auto' }}>
+        <FaVolumeUp />
         {data.length > 0 && (
           <p className="text-md font-bold">
             {data[currentAnnouncementIndex].pengumuman}
